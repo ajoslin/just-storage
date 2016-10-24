190e6d27877e4cce80cc544b75ac2ab7676bf42e
Andrew Joslin <andrewtjoslin@gmail.com>
2016-10-24 02:59:54 -0600

    Add an exports.default for ES6-friendliness

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict'
-var storage = require('has-local-storage') ?
-      require('./native-storage') :
-      require('./memory-storage')
+
+var storage = require('has-local-storage')
+  ? require('./native-storage')
+  : require('./memory-storage')
 
 module.exports = storage
+module.exports.default = storage
 
 storage.forKey = function (key) {
   var forKey = storage.bind(null, key)
