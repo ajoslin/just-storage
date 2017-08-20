eb0cc37171508c7a34fdff99d4e0395cffe17f9e
Andrew Joslin <andrewtjoslin@gmail.com>
2017-08-20 15:27:06 -0600

    Remove dependencies

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 'use strict'
 
-var storage = require('has-local-storage')
+var hasStorage = false
+try {
+  var KEY = '__JUST-STORAGE'
+  if (typeof window !== 'undefined' && window.localStorage) {
+    window.localStorage.setItem(KEY, KEY)
+    window.localStorage.removeItem(KEY)
+    hasStorage = true
+  }
+} catch (_) {}
+
+var storage = hasStorage
   ? require('./native-storage')
   : require('./memory-storage')
 
 module.exports = storage
-module.exports.default = storage
+
+storage.default = storage
 
 storage.forKey = function (key) {
   var forKey = storage.bind(null, key)
